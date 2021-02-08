2fe344436b5963e3ea84c415610dda5d12cfafce
Aaron <1546848781@qq.com>
2021-02-09 00:03:35 +0800

    可用余额为负数的可能性

--- src/main/java/_00JavaThread/Bank.java
+++ src/main/java/_00JavaThread/Bank.java
@@ -14,14 +14,17 @@ public class Bank {
     }
 
     // 存款
-    public synchronized void deposit(int m) {
+    // synchronized
+    public void deposit(int m) {
         money += m;
     }
 
     // 取款
-    public synchronized boolean withdraw(int m) {
+    // synchronized
+    public boolean withdraw(int m) {
         if (money >= m) {
             money -= m;
+            check();
             return true;// 取款成功
         } else {
             return false;// 取款失败
@@ -29,13 +32,40 @@ public class Bank {
 
     }
 
+    private void check() {
+        if (money < 0) {
+            System.out.println("可用余额为负数！ money = " + money);
+        }
+    }
+
     public String getName() {
         return name;
     }
 }
 
+class ClientThread extends Thread {
+    private Bank bank;
+
+    public ClientThread(Bank bank) {
+        this.bank = bank;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            boolean ok = bank.withdraw(1000);
+            if (ok) {
+                bank.deposit(1000);
+            }
+        }
+    }
+}
+
 class Main6 {
     public static void main(String[] args) {
-        
+        Bank bank = new Bank("A Bad Bank", 1000);
+        new ClientThread(bank).start();
+        new ClientThread(bank).start();
+
     }
 }
